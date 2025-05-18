91baec07c98d0d3642b406e90418ea9ca3463dbe
Jay <jay1904prajapati@gmail.com>
2025-05-18 15:32:46 +0530

    change the url

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,7 +2,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.LIVE_URL,
+  baseURL:
+    import.meta.env.LIVE_URL ||
+    "https://expense-tracker-2-darshans-projects-bc298955.vercel.app/api",
   // "http://localhost:5000/api",
   withCredentials: true,
   headers: {
